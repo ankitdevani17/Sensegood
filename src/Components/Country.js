e9d35bca26569ab1634ec2c64c0cda6f24e02caf
ankit <devaniankit1990@gmail.com>
2022-06-23 14:45:05 +0530

    put request on quantity done - error of total amount in country still

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
 import { useStateIfMounted } from 'use-state-if-mounted';
-var totalamount = 0; //delared globally to access amoung all functions
+var temp =0;//delared globally to access amoung all functions
 const Country = () => {
 const [country,setcountry ] =useState("IN");
-const [fdata,setdata] =useState();
+const [fdata,setdata] =useState("");
 const [Quant,SetQuant]
  = useState(0);
- const [totalamount,settotalamount] = useState("")
+ const [totalamount,settotalamount] = useState()
 useEffect(() => {
     async function getData() {
     const res = await axios.get(`/api/products/get_checkout_list/${country}/`,{mode:'cors'},{ withCredentials: true }
@@ -15,6 +15,7 @@ useEffect(() => {
     //const serverdata = JSON.stringify(res);   
     console.log(res);
     setdata(res.data);
+    console.log("data set")
       }
   let a =  getData();
   a.then(()=>{
@@ -23,30 +24,61 @@ useEffect(() => {
   })
 
 },[])
-  function initialPrice (fdata) {
-    console.log("kei bhi")
-  var temp = 0;
-  for (let i = 0; i < fdata.length; i++) {
-   temp = temp + fdata[i].quantity * fdata[i].price.amount;
+function initialPrice (fdata) {
+console.log("kei bhi")
+var temp =0;
+for (var i = 0; i < fdata.length; i++) {
+console.log("changing temp")
+temp = temp + fdata[i].quantity * fdata[i].price.amount;
+
   }
+  console.log("total amount printing")
   settotalamount(temp);
 }
 
 const Increment = (key) =>{
+  
   var ind = fdata.indexOf(key);
   fdata[ind].quantity = fdata[ind].quantity + 1;
-  totalamount = totalamount + fdata[ind].price.amount;
+  console.log(fdata[ind].id)
+  axios.put(`/api/cart/item/${fdata[ind].id}/`, {
+   quantity: fdata[ind].quantity 
+  }, {mode:'cors'},{ withCredentials: true }).then(res => {
+  console.log(res.data);
+  //alert("success");
+ // mode cors is for enabling cross-origin requests in frontend 
+})
+ 
+  .catch(err=>{
+    console.log(err);
+   // alert("Invalid Credentials");
+  })
+  temp =temp + fdata[ind].price.amount;
+ settotalamount(temp);
 if (Quant < 99){
 SetQuant(Quant+1);}
 }
 const Decrement = (key) =>{
+  
   var ind = fdata.indexOf(key);
   fdata[ind].quantity = fdata[ind].quantity - 1;
-  totalamount = totalamount - fdata[ind].price.amount;
+  axios.put(`/api/cart/item/${fdata[ind].id}/`, {
+    quantity: fdata[ind].quantity 
+   }, {mode:'cors'},{ withCredentials: true }).then(res => {
+   console.log(res.data);
+   //alert("success");
+  // mode cors is for enabling cross-origin requests in frontend 
+ })
+  
+   .catch(err=>{
+     console.log(err);
+    // alert("Invalid Credentials");
+   })
+ temp =temp - fdata[ind].price.amount;
+ settotalamount(temp);
   if (Quant > 0){
     SetQuant(Quant-1);}
 }
-
 
 var viewproducts ="";
 viewproducts = (fdata || []).map((key,index) => {
