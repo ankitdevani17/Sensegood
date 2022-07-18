d9fce94755d11d8a5d38f1a1df828453c4d9e58d
ankit <devaniankit1990@gmail.com>
2022-07-18 23:51:32 +0530

    products page done + footer changed

--- src/Components/Cardview.js
+++ src/Components/Cardview.js
@@ -32,88 +32,77 @@ const Cardview = () => {
 
   return (
     <>  
-  <section>
-    <div className="container" data-aos="zoom-in">
-        <div className="section-title">
+<section>
+    <div class="container" data-aos="zoom-in">
+        <div class="section-title">
             <h2>USB Adaptor</h2>
         </div>
-        <div className="row">
-                <div className="col-md-8">
-                <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                  <div className="carousel-inner" style={{"height":"100%"}}>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src={fmedia[1].image} alt="First slide"/>
+
+        <div class="row">
+                <div class="col-md-8">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner" style={{"height":"100%"}}>
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger5.png" alt="First slide"/>
                     </div>
-                    <div className="carousel-item active">
-                      <img className="d-block w-100" src={fmedia[1].image} alt="Second slide"/>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger.png" alt="Second slide"/>
                     </div>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger3.png" alt="Second slide"/>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger3.png" alt="Second slide"/>
                     </div>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger4.png" alt="Second slide"/>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger4.png" alt="Second slide"/>
                     </div>
-                    <div className="carousel-item">
-                      <img className="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger2.png" alt="Second slide"/>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://sensegoodinstruments.com/assets/img/charger2.png" alt="Second slide"/>
                     </div>
                   </div>
-                  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
                   </a>
-                  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
                   </a>
                 </div>
+                  
                 </div>
                 <div class="col-md-4">
                     <div>
-                        <h3>Purchase Information</h3>
-                        Mentioned total price includes GST and shipping charges for delivery in India.
-                        <br/><br/>
+                    <p>{fdata.description}</p>
+                  
                         <center>
-                      Select Quantity (packs)<br/>
-                      
-                      
-                    </center>
-                    <br/>
-                        <center>
-                          
-                            <a href="purchase.php" class="btn-explore">Buy Now</a>
+                        <button type="button" class="btn btn-primary">Buy Now</button>
                         </center>
                         <br/>
-                        <p>Contact us for delivery outside India.</p>
+                        
                     </div>
                 </div>
             </div>
-
+            
     </div>
   </section>
-  <section>
-    <div className="container">
-      <h2>Description</h2>
-      {fdata.description}
-      image 
 
-      <br/>
-      <br/>
-      {/* <ul> */}
-        {/* <li>USB charger, 5watt, universal input: ~100V to 240V, 50/60 Hz, output: 5.0Vdc, 1000 mAmps (no fast charging).</li>
-        <li>Automatic over voltage protection up to 350V</li>
-        <li>Automatic over voltage protection up to 350V</li>
-        <li>Certifications: RoHS Reach compliance, BIS, European compliance CE.</li>
-        <li>Certified standards: Meet IEC60950-1, IS13252-2010 (information technology and similar electric apparatus -safety requirement)</li>
-        <li>Tested average power efficiency: 5 star rating according to Energy Star Standard</li>
-        <li>Performed reliability tests: Long Time Charging Test, Input Over-voltage Test, Temperature/Humidity Test, Salt Spray Test, Vibration test, Insertion force test, EMC requirement (test criteria: EN55024, EN61000-4) – EFD test, Surge test, ESD test</li>
-        <li>Industry’s one of the best EMI/EMC compatible adapter with additional EM filter and RF suppressors, one of the lowest electromagnetic radiation, extremely low voltage fluctuations, extremely suppressed power line interference at 50/60Hz. It protects your device by eliminating high power RF signals getting coupled to your device’s trans-receiver. </li>
-        <li>Eliminates high voltage surges in the output, niche electronics for supplying constant stable +5V.</li>
-        <li>It has standard Indian plugs that fit appropriately in the sockets. It is Bureau of Indian Standards (BIS) certified.</li>
-        <li>Small leakage current: &lt;250 micro Amp</li>
-        <li>Insulation resistance &gt; 50Mega ohms @500VDC</li>
-        <li>Operating altitude &lt; 2000 meters</li>
-        <li>Small size, light weight</li>
-      </ul> */}
+  <section id="about" class="about">
+        <div class="container">
+            {/* <div class="section-title">
+                <h2>Description</h2>
+                <br/>
+            </div>
+            <p>{fdata.description}</p> */}
+
+  <div class="section-title">
+                <h2>About the Product</h2>
+                <br/>
+            </div>
+            <div className='write-up' dangerouslySetInnerHTML={{__html: html}}></div>
+            </div>
+  </section>
+
+ {/* start of new */}
+
       <h2>About the Product</h2>
       <div className='write-up' dangerouslySetInnerHTML={{__html: html}}>
 {/* {invoice()} */}
@@ -121,10 +110,8 @@ const Cardview = () => {
 {/* {html} */}
       <br/>
       <br/>
-    </div>
 
 {/* {writeup()} */}
-  </section>
 
     </>
 
@@ -168,23 +155,23 @@ export default Cardview
                     <div>
                         <h3>Purchase Information</h3>
                         Mentioned total price includes GST and shipping charges for delivery in India.
-                        <br><br>
+                        <br/><br/>
                         <center>
-                      Select Quantity (packs)<br>
+                      Select Quantity (packs)<br/>
                       <input type="button" name="subtract" onclick="fivehunTotal(&quot;fhqty&quot;,-1,&quot;fhtotal&quot;,1911);Total();Adddata();Discount();checkzero();" value="-" style="font-weight: bold; color: #000000; padding:15px; border:2px solid #5596CB; background-color: white; margin: 0px; border-radius: 5px;">
                       <input type="text" name="qty" id="fhqty" readonly="true" value="0" style="color: #000000; padding:15px; border:2px solid #5596CB; background-color: #ffffff; width: 60px; text-align: center; margin: 0px; border-radius: 5px;">
                       <input type="button" name="add" onclick="fivehunTotal(&quot;fhqty&quot;,1,&quot;fhtotal&quot;,1911);Total();Adddata();Discount();checkzero();" value="+" style="font-weight: bold; color: #000000; padding:15px; border:2px solid #5596CB; background-color: white;  margin: 0px;border-radius: 5px;">
-                      <br>
+                      <br/>
                       Total<input type="text" name="total" id="fhtotal" value="0" style="color: #000000; padding:10px; border:none; background-color: #ffffff;  margin: 5px; text-align: left;border-radius: 5px; width: 75px;" readonly="true">Rs.
-                      <br>
+                      <br/>
                       
                     </center>
-                    <br>
+                    <br/>
                         <center>
                           
                             <a href="purchase.php" class="btn-explore">Buy Now</a>
                         </center>
-                        <br>
+                        <br/>
                         <p>Contact us for delivery outside India.</p>
                     </div>
                 </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,70 +3,126 @@ import "./Aboutus.css"
 const Footer = () => {
   return (
     <>
-<footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
 
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>Sensegood Instruments Private Limited</h3>
-              <p>
-                Gujarat, India <br/>
+<div class="container my-5">
 
-                <strong>Phone:</strong><a href="tel:+919714179358" style={{"color": "#ffffff"}}> +91 79 8484 8002</a><br/>
-                <strong>Email:</strong> info@sensegoodinstruments.com<br/>
-              </p>
-              <div class="social-links mt-3">
-                <a href="https://www.youtube.com/channel/UCtv4DiOC89iWeWblMSbaq6Q" class="youtube"><i class="bx bxl-youtube"></i></a>
-                <a href="https://www.facebook.com/sensegoodinstruments" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="https://www.linkedin.com/company/sensegoodinstruments" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                <a href="https://twitter.com/sensegood4color" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="https://wa.me/917984848002?text=I%27m%20interested%20in%20Sensegood" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
-              </div>
-            </div>
+
+  <footer
+          class=" text-lg-start text-white"
+          style={{"backgroundColor": "#1c2331"}}
+          >
+   
+    <section
+             class="d-flex justify-content-between p-4"
+             style={{"backgroundColor": "#6351ce"}}
+             >
+     
+      <div class="me-5">
+        <span>Get connected with us on social networks:</span>
+      </div>
+   
+      <div>
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-whatsapp"></a>
+      <a href="#" class="fa fa-twitter"></a>
+      <a href="#" class="fa fa-youtube"></a>
+      <a href="#" class="fa fa-linkedin"></a>
+      </div>
+     
+    </section>
+  
+    <section class="">
+      <div class="container  text-md-start mt-5">
+        <div class="row mt-3">
+          
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          
+            <h6 class="text-uppercase fw-bold">Company name</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style={{"width": "60px" , "backgroundColor": "#7c4dff" , "height": "2px"}}
+                />
+            <p>
+              Here you can use rows and columns to organize your footer
+              content. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit.
+            </p>
           </div>
+        
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="support.php">Support</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="color-education-resources.php">Resources</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="product-accessories.php">Accessories</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="aboutus.php">About</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact Us</a></li>
-            </ul>
+            <h6 class="text-uppercase fw-bold">Products</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style={{"width": "60px" , "backgroundColor": "#7c4dff" , "height": "2px"}}
+                />
+            <p>
+              <a href="#!" class="text-white">MDBootstrap</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">MDWordPress</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">BrandFlow</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Bootstrap Angular</a>
+            </p>
           </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Products</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="spectrophotometer.php">Sensegood Spectrophotometer</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="color-measurement/software.php">SensegoodSmart Utility</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="space-defence-electronics.php">Space and Defence</a></li>
-            </ul>
+          
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+           
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style={{"width": "60px" , "backgroundColor": "#7c4dff" , "height": "2px"}}
+                />
+            <p>
+              <a href="#!" class="text-white">Your Account</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Become an Affiliate</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Shipping Rates</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Help</a>
+            </p>
           </div>
-
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Industries We Serve</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="color-measurement.php#features">Color Management </a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="space-defence-electronics.php">Space, Defence, Telecom, Industry 4.0</a></li>
-            </ul>
-
+    
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style={{"width": "60px" , "backgroundColor": "#7c4dff" , "height": "2px"}}
+                />
+            
+            <p>Gujarat, India <br/>
+            +91 79 8484 8002 <br/>
+            info@sensegoodinstruments.com</p>
           </div>
-
+     
         </div>
+      
       </div>
+    </section>
+ 
+    <div
+         class=" p-3"
+         style={{"backgroundColor": "rgba(0, 0, 0, 0.2)"}}
+         >
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/"
+         >MDBootstrap.com</a>
+        
     </div>
-
-    <div class="container">
-      <div class="copyright">
-        © <strong><span>Sensegood Instruments Private Limited</span></strong>. All Rights Reserved
-      </div>
-    </div>
+    
   </footer>
+
+</div>
+
     </>
   )
 }
